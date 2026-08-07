aa5f4da1d38560317e62c14e7a15322533727299
eleutherifer <eleutherifer@gmail.com>
2026-08-07 06:55:38 +0300

    Update consts.go

--- internal/consts.go
+++ internal/consts.go
@@ -1,7 +1,8 @@
 package internal
 
 const (
-	ApiUrl     = "https://api.devices.cloudflare.com"
+	ApiUrl     = "https://api.cloudflareclient.com"
+//    ApiUrl     = "https://api.devices.cloudflare.com"
 	ApiVersion = "v0a4471"
 	ConnectSNI   = "consumer-masque.cloudflareclient.com"
 	L4ConnectSNI = "consumer-masque-proxy.cloudflareclient.com"
